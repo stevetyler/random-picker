31f5a3479b30522d4ac92fe3ed27a1a6eee5d96a
Steve Tyler <mail@steve-tyler.co.uk>
2017-10-12 23:40:46 +0100

    updated styling

--- picker.js
+++ picker.js
@@ -2,20 +2,35 @@
 // generate random number between 0 and length of array -1
 // add picker class to div
 
+var button = document.querySelector('.button');
+var started = false;
+
+button.addEventListener('click', function() {
+  if (!started) {
+    picker();
+    started = true;
+  }
+  else {
+    //console.log('timeout id', this.timeoutID);
+    started = false;
+  }
+});
+
 function picker() {
   var divArr = document.querySelectorAll('.box');
   var interval = 75;
+  var i;
 
   (function timeout(iPrev) {
     setTimeout(function () {
+      //console.log('timeoutid', this.timeoutID);
       iPrev = iPrev || 0;
       divArr[iPrev].setAttribute('class', 'box');
-
-      var i = Math.round(Math.random() * (divArr.length - 1));
+      i = Math.round(Math.random() * (divArr.length - 1));
       divArr[i].setAttribute('class', 'picker box');
       timeout(i);
     }, interval);
   })();
 }
 
-picker();
+//picker();
